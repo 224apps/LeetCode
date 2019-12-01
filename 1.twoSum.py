9f870c303e4df79b79d1d90d462535a17cd157de
Abdoulaye Diallo <224clouds@Abdoulayes-MacBook-Pro.local>
2019-12-01 14:37:41 -0800

    added problem 1,5,7 and 8

---
+++ 1.twoSum.py
@@ -0,0 +1,17 @@
+class Solutions:
+
+def twoSum(self, nums, target):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            sum = nums[i] + nums[j]
+            if sum == target:
+                return [i,j]
+
+
+
+def longest_Substring(self, string):
+    
+def main():
+    nums [2,7,11,15]
+    
+    twoSum(nums, 9)
---
+++ 5.longest_palindromic_substring.py
@@ -0,0 +1,34 @@
+
+class Solution:
+    def longestPalindrome(self, s):
+        largestPalindrome = ""
+        for i in range(len(s)):
+            palindromeOdd = self.longestPalindromeAtIndex(s, i, i)
+            palindromeEven = self.longestPalindromeAtIndex(s,i, i + 1)
+
+            largerPalindrome = palindromeOdd if len(palindromeOdd) > len(palindromeEven) else palindromeEven
+            largestPalindrome = largerPalindrome if len(largestPalindrome) >= len(largerPalindrome) else largerPalindrome
+        return largestPalindrome
+
+    
+    def longestPalindromeAtIndex( self, s, left, right):
+        leftIndex = 0
+        rightIndex = 0
+
+        while left >= 0 and right < len(s):
+            if s[left] == s[right]:
+                leftIndex = left
+                rightIndex = right
+            else:
+                break
+                left -= 1
+                right += 1
+        return s[ leftIndex: rightIndex]
+
+
+
+class Solution:
+    def lo
+
+
+        
