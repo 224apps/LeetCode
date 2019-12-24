9c7b9d172e8218da81b3a361fa507cc897d771b8
224Apps <224apps@gmail.com>
2019-12-23 16:48:22 -0800

    LC_6 added

---
+++ 6_zigzag.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1:
+            return s
+        zigzag =  [[] for _ in range(numRows)]
+        row = 0
+        direction = -1
+        for char in s:
+            zigzag[row].append(char)
+            if row == 0 or  row == numRows - 1:
+                direction = -direction
+            row += direction
+        return ''.join([c for r in zigzag for c in r])
+
+        
