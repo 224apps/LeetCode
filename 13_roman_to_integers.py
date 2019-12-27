fcdc7d2e36b51281b7e84127c024cd78b98304ab
224Apps <224apps@gmail.com>
2019-12-26 23:56:51 -0500

    LC_13 added

---
+++ 13_roman_to_integers.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+        res = 0
+        for i, c in enumerate(s):
+            if i < len(s) - 1 and roman[c] < roman[s[i + 1]]:
+                res -= roman[c]
+            else:
+                res += roman[c]
+        return res
