ddc64fdccaee3f2a6f15caa258e2ccb34d2eca14
224Apps <224apps@gmail.com>
2020-02-26 15:43:39 -0800

    Added LC-128 and LC-238

---
+++ lc-128-longestConsecutive.py
@@ -0,0 +1,21 @@
+
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+
+        result = 0
+
+        nums = set(nums)
+        for num in nums:
+            if num -1 not in nums:
+                curr= num
+                length = 1
+                while curr + 1 in nums:
+                    curr += 1
+                    length += 1
+                result = max(length, result)
+        return result
