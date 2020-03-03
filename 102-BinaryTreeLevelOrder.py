cd6298a7daba40c27810460434ef2099f9d6005a
224Apps <224apps@gmail.com>
2020-03-03 15:31:04 -0800

    Added LC-102

---
+++ 102-BinaryTreeLevelOrder.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        queue = [(root,0)]
+        levelMap = {}
+        
+        while queue:
+            node, level = queue.pop(0)
+            if node.left:
+                queue.append((node.left, level + 1))
+            if node.right:
+                queue.append((node.right, level + 1))
+            if level in levelMap:
+                levelMap[level].append(node.val)
+            else:
+                levelMap[level] = [node.val]
+        result = []
+        for key, value in levelMap.iteritems():
+        	result.append(value)
+        return result
