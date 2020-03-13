9fb65d6fe38f576e9504ef9ec56c721695a64c6a
224Apps <224apps@gmail.com>
2020-03-12 18:14:16 -0700

    Added LC 162, 278, 33, 454, 94, 98

--- 69Sqrt.py
+++ 69Sqrt.py
@@ -1,16 +1,55 @@
+'''
+mplement int sqrt(int x).
+
+Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+
+Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
+
+Example 1:
+
+Input: 4
+Output: 2
+Example 2:
+
+Input: 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since 
+             the decimal part is truncated, 2 is returned.
+'''
+
+
 class Solution(object):
     def mySqrt(self, x):
         """"
         :type x: int
         :rtype: int
         """
-        if x == 0 or x == 1:
-            return x
-        s = 1
-        while True:
-            if s * s > x:
-                return s-1
-            s += 1
-        return -1
+        
+        left, right = 0, x
+        while left <= right:
+            mid = left + (right - left) // 2
             
+            if mid * mid > x:
+                right = mid - 1
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                return mid
+        return right
     
+
+# class Solution(object):
+#     def mySqrt(self, x):
+#         """"
+#         :type x: int
+#         :rtype: int
+#         """
+#         if x == 0 or x == 1:
+#             return x
+#         s = 1
+#         while True:
+#             if s * s > x:
+#                 return s-1
+#             s += 1
+#         return -1
+            
