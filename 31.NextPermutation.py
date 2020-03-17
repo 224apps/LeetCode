a17278ca1a869822dd3058680c2f74d01ce95cb2
224Apps <224apps@gmail.com>
2020-03-16 18:49:30 -0700

    Added Lc 31, 32, 39, 57, 67

---
+++ 31.NextPermutation.py
@@ -0,0 +1,37 @@
+'''
+Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+
+If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+
+The replacement must be in-place and use only constant extra memory.
+
+Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+
+1,2,3 → 1,3,2
+3,2,1 → 1,2,3
+1,1,5 → 1,5,1
+'''
+
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) -2
+        
+        while i >= 0 and nums[i] >= nums[i+1]:
+            i -= 1
+            
+        if i >= 0:
+            j = len(nums) - 1
+            while j >= i and nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j] , nums[i]
+        start = i + 1
+        end = len(nums) - 1
+        
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
