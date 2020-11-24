59b711975f22d45194e9fb74526b8f839bbd7dd0
224Apps <224apps@gmail.com>
2020-11-24 18:35:46 -0500

    Add 437.py

---
+++ 400-500/437.py
@@ -0,0 +1,56 @@
+'''
+You are given a binary tree in which each node contains an integer value.
+
+Find the number of paths that sum to a given value.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+
+The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+
+Example:
+
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+Return 3. The paths that sum to 8 are:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3. -3 -> 11
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> int:
+        count = 0
+        
+        def dfs(node, currSum):
+            nonlocal count
+            if node is None:
+                return
+            if currSum + node.val == sum:
+                count += 1
+            dfs(node.left, currSum + node.val)
+            dfs(node.right, currSum + node.val)
+        
+        
+        def starter_dfs(node):
+            if node is None:
+                return
+            dfs(node,0)
+            starter_dfs(node.left)
+            starter_dfs(node.right)
+            
+        starter_dfs(root)
+        return count
