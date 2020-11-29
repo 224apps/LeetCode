7d1407f0cd7307c4f027bad1027030a50027cd6c
224Apps <224apps@gmail.com>
2020-11-29 07:36:36 -0500

    Added 1290.py

--- 1200-1300/1290.py
+++ 1200-1300/1290.py
@@ -32,15 +32,30 @@ Number of nodes will not exceed 30.
 Each node's value is either 0 or 1.
 '''
 # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def getDecimalValue(self, head: ListNode) -> int:
+#         res = ""
+        
+#         while head:
+#             res += str(head.val)
+#             head = head.next
+#         return int(res, 2)
+# Definition for singly-linked list.
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+        def __init__(self, val=0, next=None):
+                self.val = val
+                self.next = next
 class Solution:
     def getDecimalValue(self, head: ListNode) -> int:
-        res = ""
         
-        while head:
-            res += str(head.val)
-            head = head.next
-        return int(res, 2)
+        num = head.val
+        
+        while head.next:
+                num = num * 2 + head.next.val
+                head = head.next
+        return num
+                
